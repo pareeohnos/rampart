eb4b8aec01d614c5423040573cae45b3e2b7072a
Adrian Hooper <adrian@euphoricpanda.co.uk>
2017-04-17 00:11:01 +0100

    Bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rampart.Mixfile do
   def project do
     [
       app: :rampart,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
